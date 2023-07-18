8f0146dc4de07d5881c166319615f9bb08975b7c
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-07-18 16:52:50 +0000

    todo ok

--- src/js/views/CharactersDetails.js
+++ src/js/views/CharactersDetails.js
@@ -33,7 +33,7 @@ export const CharactersDetails = () =>{
 
     return(<>
         <div className="d-flex" >
-            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.hdwallpapers.in%2Fwalls%2Fstar_wars_episode_vii_the_force_awakens-wide.jpg&f=1&nofb=1&ipt=f1b374543aa1407ddb2164b42def109ac7d50383b136fb82d6ff3435fd7ddf87&ipo=images" id="foto" className="img-fluid m-5" alt="..."/>
+            <img src={`https://starwars-visualguide.com/assets/img/characters/${params.uid}.jpg`} id="foto" className="img-fluid m-5" alt="..."/>
             
             <div  className="mx-5 mt-5">
 
